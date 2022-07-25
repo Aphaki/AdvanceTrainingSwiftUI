f162e2399b249b744fd98c1576a8408361664b7d
Aphaki <kiapha1108@gmail.com>
2022-07-25 18:05:27 +0900

    Initial Commit

---
+++ AdvanceTrainingSwiftUI/AdvanceTrainingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvanceTrainingSwiftUIApp.swift
+//  AdvanceTrainingSwiftUI
+//
+//  Created by Sy Lee on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AdvanceTrainingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
