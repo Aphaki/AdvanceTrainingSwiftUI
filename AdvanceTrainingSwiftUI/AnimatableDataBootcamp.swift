92257928647676282eba89abe5f2c06a7630e794
Aphaki <kiapha1108@gmail.com>
2022-08-03 01:09:00 +0900

    AnimatableData 수정

--- AdvanceTrainingSwiftUI/AnimatableDataBootcamp.swift
+++ AdvanceTrainingSwiftUI/AnimatableDataBootcamp.swift
@@ -37,19 +37,74 @@ struct QuaterCornerRadiusRectangle: Shape {
     }
 }
 
+struct MyRoundedRectangle: Shape {
+    
+    var cornerRadius: CGFloat
+    
+    var animatableData: CGFloat {
+        get { cornerRadius }
+        set { cornerRadius = newValue }
+    }
+
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX + cornerRadius, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - cornerRadius, y: rect.minY))
+            path.addArc(center: CGPoint(x: rect.maxX - cornerRadius,
+                                        y: rect.minY + cornerRadius),
+                        radius: cornerRadius,
+                        startAngle: Angle(degrees: 270),
+                        endAngle: Angle(degrees: 0),
+                        clockwise: false)
+            
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerRadius))
+            path.addArc(center: CGPoint(x: rect.maxX - cornerRadius,
+                                        y: rect.maxY - cornerRadius),
+                        radius: cornerRadius,
+                        startAngle: Angle(degrees: 0),
+                        endAngle: Angle(degrees: 90),
+                        clockwise: false)
+            
+            path.addLine(to: CGPoint(x: rect.minY + cornerRadius, y: rect.maxY))
+            path.addArc(center: CGPoint(x: rect.minX + cornerRadius,
+                                        y: rect.maxY - cornerRadius),
+                        radius: cornerRadius,
+                        startAngle: Angle(degrees: 90),
+                        endAngle: Angle(degrees: 180),
+                        clockwise: false)
+            
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + cornerRadius))
+            path.addArc(center: CGPoint(x: rect.minX + cornerRadius,
+                                        y: rect.minY + cornerRadius),
+                        radius: cornerRadius,
+                        startAngle: Angle(degrees: 180),
+                        endAngle: Angle(degrees: 270),
+                        clockwise: false)
+        }
+    }
+}
+
+
+
 struct AnimatableDataBootcamp: View {
     
-    @State var animate: Bool = false
+    @State var isCircle: Bool = false
     
     var body: some View {
-        
-        QuaterCornerRadiusRectangle(cornerRadius: animate ? 40 : 0)
-            .frame(width: 150, height: 150)
-            .onAppear {
-                withAnimation(Animation.easeInOut(duration: 2).repeatForever()) {
-                    animate.toggle()
-                }
+        VStack {
+            RoundedRectangle(cornerRadius: isCircle ? 75 : 0)
+                .frame(width: 150, height: 150)
+            
+            MyRoundedRectangle(cornerRadius: isCircle ? 75 : 0)
+                .frame(width: 150, height: 150)
+            
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 2).repeatForever()) {
+                isCircle.toggle()
             }
+        }
     }
 }
 
